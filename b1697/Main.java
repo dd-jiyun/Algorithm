e28fa948236a07eb9669159744ac9182216f852c
dd-jiyun <dlwldbs8015@gmail.com>
2024-07-18 23:52:31 +0900

    [beakjoon] 1697번 - 숨바꼭질 (수정)

--- b1697/Main.java
+++ b1697/Main.java
@@ -9,9 +9,8 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, K;
-    static int idx[] = {-1, 1, 2};
+    static int[] idx = {-1, 1};
     static boolean[] visited = new boolean[100001];
-    static int cnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,31 +20,32 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
 
         System.out.println(bfs(N));
-
     }
 
     public static int bfs(int cur) {
+        if (cur == K) return 0;  // 시작점과 목표가 같을 경우
+
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(cur);
         visited[cur] = true;
+        int cnt = 0;
 
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
-                int currentN = queue.remove();
+                int currentN = queue.poll();
                 if (currentN == K) {
                     return cnt;
                 }
-                for (int j = 0; j < 3; j++) {
-                    int next;
-                    if (j < 2) {
-                        next = currentN + idx[j];
-                    } else {
-                        next = currentN * idx[j];
+                for (int j = 0; j < 2; j++) {
+                    int next = currentN + idx[j];
+                    if (next >= 0 && next <= 100000 && !visited[next]) {
+                        visited[next] = true;
+                        queue.offer(next);
                     }
-                    if (next < 0 || next > 100000 || visited[next]) {
-                        continue;
-                    }
+                }
+                int next = currentN * 2;
+                if (next <= 100000 && !visited[next]) {
                     visited[next] = true;
                     queue.offer(next);
                 }
