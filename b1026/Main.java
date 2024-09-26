2fea4d906d29d9e4fa1f21114c12322974beabdc
dd-jiyun <dlwldbs8015@gmail.com>
2024-09-26 22:06:37 +0900

    [baekjoon] 1026번 - 보물 (수정)
    - 내림차순 정렬을 `Collections.reverseOrder()` 사용했습니다.

--- b1026/Main.java
+++ b1026/Main.java
@@ -4,12 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N;
     static int[] A;
-    static int[] B;
+    static Integer[] B;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,7 +18,7 @@ public class Main {
         N = Integer.parseInt(br.readLine());
 
         A = new int[N];
-        B = new int[N];
+        B = new Integer[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
@@ -31,12 +32,11 @@ public class Main {
 
         Arrays.sort(A);
 
-        Integer[] B_sorted = Arrays.stream(B).boxed().toArray(Integer[]::new);
-        Arrays.sort(B_sorted, (x, y) -> y - x);
+        Arrays.sort(B, Collections.reverseOrder());
 
         int result = 0;
         for (int i = 0; i < N; i++) {
-            result += A[i] * B_sorted[i];
+            result += A[i] * B[i];
         }
 
         System.out.println(result);
