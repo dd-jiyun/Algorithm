8326c8b5ca2f09a8dd8a4dfcbc11473c84433957
dd-jiyun <dlwldbs8015@gmail.com>
2024-09-30 17:23:54 +0900

    [programmers] 프로그래머스 - 더 맵게

---
+++ p42626/Main.java
@@ -0,0 +1,44 @@
+package p42626;
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for (int i : scoville) {
+            queue.add(i);
+        }
+
+        while (queue.peek() < K) {
+
+            if (queue.size() < 2) {
+                return -1;
+            }
+
+            // 가장 작은 두 요소를 섞음
+            int first = queue.poll();
+            int second = queue.poll();
+            int mix = first + (second * 2);
+
+            queue.add(mix);
+            answer++;
+        }
+
+        return answer;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.solution(new int[]{1, 2, 3, 9, 10, 12}, 7));
+
+        System.out.println(solution.solution(new int[]{1, 2, 3}, 35));
+    }
+}
