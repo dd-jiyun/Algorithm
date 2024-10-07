bd935a52499f45d0bfa42909172c7ec59a1ff03f
dd-jiyun <dlwldbs8015@gmail.com>
2024-10-07 23:56:12 +0900

    [baekjoon] 9093번 - 단어 뒤집기 v2

--- b9093/Main.java
+++ b9093/Main.java
@@ -3,20 +3,22 @@ package b9093;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder result = new StringBuilder();
 
-        int num = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < num; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            while (st.hasMoreTokens()) {
-                StringBuilder sb = new StringBuilder(st.nextToken());
-                result.append(sb.reverse()).append(" ");
+        for (int i = 0; i < T; i++) {
+            String[] words = br.readLine().split(" ");
+
+            for (String word : words) {
+                for (int j = word.length() - 1; j >= 0; j--) {
+                    result.append(word.charAt(j));
+                }
+                result.append(" ");
             }
             result.append("\n");
         }
