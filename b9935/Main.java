163170ad96fb390038ac242b9f9e6b021c92268f
dd-jiyun <dlwldbs8015@gmail.com>
2025-01-17 16:58:49 +0900

    [baekjoon] 9935번 - 문자열 폭발

---
+++ b9935/Main.java
@@ -0,0 +1,45 @@
+package b9935;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        String bomb = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+
+            if (stack.size() >= bomb.length()) {
+                boolean isBomb = true;
+                int startIdx = stack.size() - bomb.length();
+
+                for (int j = 0; j < bomb.length(); j++) {
+                    if (stack.get(startIdx + j) != bomb.charAt(j)) {
+                        isBomb = false;
+                        break;
+                    }
+                }
+
+                if (isBomb) {
+                    for (int j = 0; j < bomb.length(); j++) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : stack) {
+            sb.append(c);
+        }
+
+        System.out.println(sb.isEmpty() ? "FRULA" : sb);
+        br.close();
+    }
+}
